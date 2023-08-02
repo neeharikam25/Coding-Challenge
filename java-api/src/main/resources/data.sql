45112bf9ffaadaeb9d591d52a65c2233c5b4e315
Alexandru <raducanu.alexandru2001@gmail.com>
2023-08-02 14:22:35 +0100

    Done the schema.sql file

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,18 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+INSERT INTO Users (user_id, name, email) VALUES (1, 'Alex', 'alex@email');
+INSERT INTO Users (user_id, name, email) VALUES (2, 'Neeharika', 'neeharika@email');
+INSERT INTO Users (user_id, name, email) VALUES (3, 'Teodora', 'teodora@email');
+
+INSERT INTO Book (book_id, name) VALUES (1, 'trading_book_1');
+INSERT INTO Book (book_id, name) VALUES (2, 'trading_book_2');
+INSERT INTO Book (book_id, name) VALUES (3, 'trading_book_3');
+INSERT INTO Book (book_id, name) VALUES (4, 'trading_book_4');
+INSERT INTO Book (book_id, name) VALUES (5, 'trading_book_5');
+INSERT INTO Book (book_id, name) VALUES (6, 'trading_book_6');
+
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 1, 1);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 2, 1);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 3, 1);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 4, 1);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 1, 2);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 2, 2);
+INSERT INTO BookUser ( book_id, user_id) VALUES ( 3, 2);
