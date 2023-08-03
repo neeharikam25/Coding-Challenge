3b3d3742c97b9c654b4726ddaf083fa0b14f089e
Alexandru <raducanu.alexandru2001@gmail.com>
2023-08-03 16:40:59 +0300

    Did the Navbar, no links on buttons. Also, the <hello, user> message is hard-coded

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
+import { Routes } from "react-router-dom";
+
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
-      <Navigation/>
-      <Routes>
-        
-      </Routes>
+      <Navigation />
+      <Routes></Routes>
     </>
   );
 }
--- react-app/src/components/Navigation.jsx
+++ react-app/src/components/Navigation.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+
 const Navigation = () => {
   const [activeUrl, setActiveUrl] = useState("");
+//   const [activeUser, setActiveUser] = useState("");
+
   const appLocation = useLocation();
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     setActiveUrl(window.location.pathname);
@@ -21,23 +26,33 @@ const Navigation = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Heroes</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">All Heroes</Nav.Link>
-            <Nav.Link href="#link">Add Hero</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-      <Button id="sign-out"> Sign out </Button>
+        {/* <Card.Text
+          style={{
+            fontSize: "20px",
+            marginLeft: "15%",
+            marginRight: "15%",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {" "}
+          Hello, user!
+        </Card.Text> */}
 
-      {/* add the second button ( Go back to Main Page ) only if we are not in the main page */}
-      {activeUrl === "/" ? (
-        <Button id="sign-out"> Back to Main Page </Button>
-      ) : (
-        <></>
-      )}
+        <Navbar.Text href="/">Hello, user!</Navbar.Text>
+
+        <Navbar.Brand href="/">My Bonds App</Navbar.Brand>
+
+        <div id="navbar_buttons">
+          <Button id="signout_button"> Sign out </Button>
+
+          {/* add the second button ( Go back to Main Page ) only if we are not in the main page */}
+          {activeUrl !== "/" ? (
+            <Button id="back_button"> Back to Main Page </Button>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Container>
     </Navbar>
   );
 };
