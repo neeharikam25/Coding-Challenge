1edec1ec1a9aa864fe589256e90381f17b8025fd
Virginia-Mădălina Chirca <chirca_virginia@yahoo.com>
2023-08-04 12:25:26 +0300

    bla

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Login/>} />
 
-          <Route path="/allbonds" element={<AllBonds/>} />
+          <Route path="/allBonds" element={<AllBonds/>} />
           <Route path='/seeTrades' element={<AllTrades/>}/>
 
         </Routes>
