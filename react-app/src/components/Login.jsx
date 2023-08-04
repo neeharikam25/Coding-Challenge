7192995457255b89ff2f5df1c6cbd8df2865578c
teodoradumitr7 <teodoradt19@gmail.com>
2023-08-04 11:28:59 +0300

    added login i guess

--- react-app/src/components/Login.jsx
+++ react-app/src/components/Login.jsx
@@ -2,63 +2,73 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { Navigate,useNavigate } from 'react-router-dom';
+import { ToastContainer,toast } from "react-toastify";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../config/firebase' 
 
 const Login = () => {
-  const [data, setData] = useState({
-    username: "",
-    password: "",
-  });
+  // const [data, setData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
   const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value });
-  };
+  // const handleChange = ({ currentTarget: input }) => {
+  //   setData({ ...data, [input.name]: input.value });
+  // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const url = "http://localhost:8090/login";
-      await axios.post(url, data);
-      localStorage.setItem("token", data.username);
-      window.location = "/";
-    } catch (err) {
-      if (
-        err.response &&
-        err.response.status >= 400 &&
-        err.response.status <= 500
-      ) {
-        setError(err.response.data.message);
+  const handleSubmit = async () => {
+    if(email===""||password===""){
+      toast.error('Email and password are req');
+    }
+    try{
+      await signInWithEmailAndPassword(auth,email,password).then(()=>{
+        navigate('/allbonds');
+      })
+    }
+    catch(err){
+      if(err.code==='auth/invalid-email'){
+        toast.error('Invalid email id')
+      }
+      if(err.code==='auth/user-not-found'){
+        toast.error('Please check your email')
+      }
+      if(err.code==='auth/wrong-password'){
+        toast.error('Please check your password')
+      }
+      if(err.code==='auth/too-many-requests'){
+        toast.error('Too many attempts, try again later')
       }
     }
+
   };
-  const navigate=useNavigate();
+
+
+ 
   return (
     <div id="login">
       <h1>Log In</h1>
       <form id="form-container" onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
+        <label htmlFor="username">Email</label>
         <input
-          type="text"
-          name="username"
-          id="username"
-          onChange={handleChange}
-          defaultValue="dum"
-          required
+         type="text"
+         placeholder="Email"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
         />
         <label htmlFor="password">Password</label>
         <input
-          type="password"
-          name="password"
-          id="password"
-          onChange={handleChange}
-          value={data.password}
-          required
+           type="password"
+           placeholder="Password"
+           value={password}
+           onChange={(e) => setPassword(e.target.value)}
         />
         {error && <div className="errorSquare">{error}</div>}
         <div id="buttons">
-          <button type="submit" onClick={() => {
-              navigate('/allBonds');
-            }}>Log In</button>
+          <button type="submit" onClick={handleSubmit}>Log In</button>
         </div>
       </form>
     </div>
---
+++ react-app/src/config/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDl_NAMv8tTAixor8eilzofDkVzdJvMuAk",
+  authDomain: "codingchallenge-d04df.firebaseapp.com",
+  projectId: "codingchallenge-d04df",
+  storageBucket: "codingchallenge-d04df.appspot.com",
+  messagingSenderId: "1095925214503",
+  appId: "1:1095925214503:web:94e40959e31a5a2de70d02",
+  measurementId: "G-3B3R0X3P4Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db=getFirestore(app)
+const auth=getAuth(app)
+
+export  {auth,db}
