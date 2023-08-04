a3ead80333ade396df98fea031c63937df9ad563
Alexandru <raducanu.alexandru2001@gmail.com>
2023-08-04 16:08:34 +0300

    Datepicker

--- react-app/src/components/AllBonds.jsx
+++ react-app/src/components/AllBonds.jsx
@@ -1,49 +1,75 @@
-import React from 'react'
-import BondDetails from './BondDetails'
-import Row from 'react-bootstrap/Row'
-import { useState, useEffect} from 'react'
-import { getAllBonds } from '../services/BondServices'
-import { DatePicker } from '@mui/x-date-pickers'
+import React from "react";
+import BondDetails from "./BondDetails";
+import Row from "react-bootstrap/Row";
+import { useState, useEffect } from "react";
+import { getAllBonds } from "../services/BondServices";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import { differenceInDays } from "date-fns";
 
 const AllBonds = () => {
+  const [bonds, setBonds] = useState([]);
 
+  //const [value, setValue] = useState(new Date(2021, 0, 3, 0, 0, 0));
 
-  const [bonds,setBonds] = useState([]);
+  const [value, setValue] = useState();
 
-  useEffect(()=>{
-    getBondsFromAPI();}, 
-    []
-  );
+  const handlerChange = (newValue) => {
+    setValue(newValue.$d);
+  };
 
-  const getBondsFromAPI = ()=>{
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
+  useEffect(() => {
+    getBondsFromAPI();
+  }, []);
+
+  const getBondsFromAPI = () => {
     getAllBonds()
-    .then(res => {
+      .then((res) => {
         setBonds(res.data);
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
         setBonds([]);
         console.log(err);
-    })
-  }
-    
+      });
+  };
+
   return (
     <>
-    <DatePicker label="Bond Maturity Date" />
-    <div className='list-bonds'>
-      <div className='bonds-center'>
-      <Row className='bond-column'>
-        {bonds.map(bond => (
-            <div className='container' key={bond.securityId}>
-              <BondDetails info={bond}  />
-            </div>
-          ))}
-      </Row>
-      </div>
+      <DatePicker
+        label="Bond maturity date"
+        onChange={(newValue) => {
+          handlerChange(newValue);
+        }}
+      />
+
+      <div className="list-bonds">
+        <div className="bonds-center">
+          <Row className="bond-column">
+            {bonds.map((bond) => {
+              if (
+                differenceInDays(new Date(bond.bondMaturityDate), value) <= 5 &&
+                differenceInDays(new Date(bond.bondMaturityDate), value) >= -5
+              )
+                return (
+                  <div className="container" key={bond.securityId}>
+                    <BondDetails info={bond} />
+                    {console.log(
+                      differenceInDays(new Date(bond.bondMaturityDate), value)
+                    )}
+                  </div>
+                );
+
+              return <div key={bond.securityId}></div>;
+            })}
+          </Row>
+        </div>
       </div>
     </>
-  )
+  );
+};
 
-
-}
-
-export default AllBonds
+export default AllBonds;
