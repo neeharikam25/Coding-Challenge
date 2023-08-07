55b5c8708677064e183ea54582f2e8a6ddbc6b8a
Alexandru <raducanu.alexandru2001@gmail.com>
2023-08-07 15:43:44 +0300

    Added TradeDetails table row structure

--- react-app/src/components/TradeDetails.jsx
+++ react-app/src/components/TradeDetails.jsx
@@ -1,58 +1,52 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import { useState,useEffect } from 'react';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { getAllTrades,getTradesCP } from '../services/TradeServices'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { getAllTrades, getTradesCP } from "../services/TradeServices";
 
+const TradeDetails = (props) => {
+  const [counter, setCounter] = useState(0);
+  const [cp, setCp] = useState("");
 
+  const [ok, setOk] = useState(false);
+  useEffect(() => {
+    getCPFromAPI();
+  }, []);
 
-
-const TradeDetails = (props ) => {
-  const[counter, setCounter] = useState(0);
- const [cp,setCp]=useState('');
- 
-
-  const [ok,setOk]=useState(false)
-  useEffect(()=>{
-    getCPFromAPI();}, 
-    []
-  );
-  
-  const getCPFromAPI = (arg)=>{
+  const getCPFromAPI = (arg) => {
     getTradesCP(arg)
-    .then(res => {
+      .then((res) => {
         setCp(res.data);
-        console.log(res.data)
-    })
-    .catch(err => {
-        
+        console.log(res.data);
+      })
+      .catch((err) => {
         console.log(err);
-    })
-  }
-getCPFromAPI(props.info.counterparty_id);
+      });
+  };
+  getCPFromAPI(props.info.counterparty_id);
   return (
-    <Card id="bond">
-      <Card.Body>
-        <Card.Title>Trade {props.info.trade_id}</Card.Title>
-        <Card.Title>ISIN : {props.isin}</Card.Title>
-        <Card.Title>Book : Book{props.book}</Card.Title>
-        <Card.Title>
-          Trade settlement date: {props.info.trade_settlement_date}
-        </Card.Title>
-        <Card.Title>Trade date: {props.info.trade_date}</Card.Title>
-        <Card.Title id="status" style={{ color: "#008000" }}>
-          Status: {props.info.status}
-        </Card.Title>
-        <Card.Title>Currency: {props.info.currency}</Card.Title>
-        <Card.Title>Unit price: {props.info.unit_price}</Card.Title>
-        <Card.Title>Buy/Sell : {props.info.type}</Card.Title>
-        <Card.Title>Quantity : {props.info.quantity}</Card.Title>
-        <Card.Title>Counter Party : {cp}</Card.Title>
-      </Card.Body>
-    </Card>
-  );
-}
+    // <div className="list-bonds">
+    //     <div className="bonds-center">
+    //       <Row className="bond-column">
 
-export default TradeDetails
+    <tr>
+      <td>{props.info.trade_id}</td>
+      <td>{props.info.trade_settlement_date}</td>
+      <td>{props.info.trade_date}</td>
+      <td style={{ color: "#008000" }}>{props.info.status}</td>
+      <td>{props.info.currency}</td>
+      <td>{props.info.unit_price}</td>
+      <td>{props.info.type}</td>
+      <td>{props.info.quantity}</td>
+      <td>{cp}</td>
+    </tr>
+
+    // </Row>
+    // </div>
+    // </div>
+  );
+};
+
+export default TradeDetails;
